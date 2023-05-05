4e99278c6c32946c3977b6c49813d9611853d970
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-05-05 19:42:09 +0300

    changes

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
+import { getCategories } from "./JS/getcategories.js";
+
 const listOfCategories = document.querySelector(".categories-list");
 
-fetch("https://books-backend.p.goit.global/books/category-list")
-  .then((response) => {
-    return response.json();
-  })
-  .then((categories) =>
-    categories.map((categorie) => {
-      listOfCategories.insertAdjacentHTML(
-        "beforeend",
-        `<li class="categories-item">${categorie.list_name}</li>`
-      );
-    })
-  )
-  .catch((error) => {
-    console.log(error);
+getCategories().then((response) => {
+  response.map((categorie) => {
+    listOfCategories.insertAdjacentHTML(
+      "beforeend",
+      `<li class="categories-item">${categorie.list_name}</li>`
+    );
   });
+});
