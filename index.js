7db52880121b8057e7ff8fc57415e844ce7f6026
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-05-06 20:00:50 +0300

    from css to scss

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import { getCategories } from "./JS/getcategories.js";
 
-const listOfCategories = document.querySelector(".categories-list");
+const listOfCategories = document.querySelector(".categories__list");
 
 getCategories().then((response) => {
   response.map((categorie) => {
     listOfCategories.insertAdjacentHTML(
       "beforeend",
-      `<li class="categories-item">${categorie.list_name}</li>`
+      `<li class="categories__item">${categorie.list_name}</li>`
     );
   });
 });
 
 if (getCategories) {
-  const test = document.querySelectorAll(".categories-item");
+  const test = document.querySelectorAll(".categories__item");
   test.forEach(() => addEventListener("click", () => console.log("click")));
 }
